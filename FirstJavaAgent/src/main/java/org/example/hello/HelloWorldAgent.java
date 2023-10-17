b48423daa38989c19ed67799a216a835665f818d
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2023-10-17 15:15:10 +0300

    initializing my first Java agent

--- FirstJavaAgent/src/main/java/org/example/hello/HelloWorldAgent.java
+++ FirstJavaAgent/src/main/java/org/example/hello/HelloWorldAgent.java
@@ -4,20 +4,22 @@ import jade.core.Agent;
 import jade.core.Location;
 import java.lang.String;
 
-public abstract class HelloWorldAgent extends Agent implements Location {
+public abstract class HelloWorldAgent extends Agent  {
 
     protected void setup() {
+
+        Location location = here();
         System.out.println("Agent Started: Hello World!");
         System.out.println("-----About Me:-----");
         System.out.println("My local name is: " + getLocalName());
         System.out.println("My globally unique name is: " + getName());
         System.out.println("-----About Here:-----");
-        System.out.println("I am running in a location called:" + getAddress());
-        System.out.println("Which is identified uniquely as: " + getID());
-        System.out.println("And is contactable at: " + getAddress());
-        System.out.println("Using the protocol: " + getProtocol());
+        System.out.println("I am running in a location called:" + location.getAddress());
+        System.out.println("Which is identified uniquely as: " + location.getID());
+        System.out.println("And is contactable at: " + location.getAddress());
+        System.out.println("Using the protocol: " + location.getProtocol());
 
         //make this agent terminate
-        doDelete();
+        //doDelete();
     }
 }
